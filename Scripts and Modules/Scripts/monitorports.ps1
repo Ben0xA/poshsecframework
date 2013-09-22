7fb0f94a2020766c4f2b2ee7d9417b347c23af8b
Ben Ten <web@ben0xa.com>
2013-09-22 16:07:59 -0500

    Updated MonitorPorts
    Added Whitelist of Ports and ProcessNames
    Fixed output of the alert to make it more intuitive.
    New ports are added to the baseline
    Closed ports are now detected and removed from baseline

--- Scripts and Modules/Scripts/monitorports.ps1
+++ Scripts and Modules/Scripts/monitorports.ps1
@@ -18,10 +18,11 @@ Import-Module $PSFramework
 [boolean]$scan = $True;
 $baseline = @()
 $active = @()
+$remotewhitelist = @(0)
+$processwhitelist = @("ssh-agent", "firefox", "tweetdeck", "thunderbird", "Idle")
 
 $PSStatus.Update("Setting a baseline.")
 $baseline = Get-SecOpenPorts
-
 do
 {
   $PSStatus.Update("Pausing for 5 seconds")
@@ -31,20 +32,48 @@ do
   $active = Get-SecOpenPorts
   
   $rslts = Compare-SecOpenPort $baseline $active
-  $rslts = $rslts | Where-Object { $_.SideIndicator -eq '=>' }
   
   foreach($rslt in $rslts)
   {
-    if($rslt.InputObject.State="ESTABLISHED")
+    if(($rslt.InputObject.State="ESTABLISHED") -and
+        ($rslt.SideIndicator -eq "=>") -and
+        ($remotewhitelist -notcontains $rslt.InputObject.RemotePort) -and
+        ($processwhitelist -notcontains $rslt.InputObject.ProcessName))
     {
       $protocol = $rslt.InputObject.Protocol
       $local = $rslt.InputObject.LocalAddress + ":" + $rslt.InputObject.LocalPort
       $remote = $rslt.InputObject.RemoteAddress + ":" + $rslt.InputObject.RemotePort
       $pname = $rslt.InputObject.ProcessName
       
-      $PSAlert.Add("New port detected. $protocol $($local)<=>$($remote) ($pname)", 3)
-      $baseline += $rslt
-    }  
+      $PSAlert.Add("Port Opened: $protocol $($local)<=>$($remote) ($pname)", 2)
+      $baseline += $rslt.InputObject
+    }
+    if(($rslt.SideIndicator -eq "<=") -and
+        ($remotewhitelist -notcontains $rslt.InputObject.RemotePort) -and
+        ($processwhitelist -notcontains $rslt.InputObject.ProcessName))
+    {
+      $protocol = $rslt.InputObject.Protocol
+      $local = $rslt.InputObject.LocalAddress + ":" + $rslt.InputObject.LocalPort
+      $remote = $rslt.InputObject.RemoteAddress + ":" + $rslt.InputObject.RemotePort
+      $pname = $rslt.InputObject.ProcessName
+      
+      $PSAlert.Add("Port Closed: $protocol $($local)<=>$($remote) ($pname)",0)
+      
+      # You can't remove items from an array. You have to rebuild it.
+      [int]$blidx = 0
+      $newbl = @()
+      do
+      {
+        $blobj = $baseline[$blidx]
+        if($blobj -ne $rslt.InputObject)
+        {
+            $newbl += $blobj
+        }
+        $blidx++
+      } while (($blidx -lt $baseline.count))
+      $baseline = $newbl
+      $newbl = $null
+    }
   }
 } while ($scan)
 
