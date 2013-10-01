20771cd50a451b6fadf2b7c12c4d761cdbb3bb7a
Ben0xA <web@ben0xa.com>
2013-10-01 06:08:40 -0500

    Moving to PoshSec/PoshSecFramework
    Doing this to retain change history but to have only one repo.

--- Scripts and Modules/Modules/PoshSecFramework/Baselines/Baselines.psm1
+++
@@ -1,2 +0,0 @@
-﻿. (Join-Path $PSScriptRoot Start-SecBaselines.ps1)
-. (Join-Path $PSScriptRoot Start-SecDailyFunctions.ps1)
--- Scripts and Modules/Modules/PoshSecFramework/Log-Management/Log-Management.psm1
+++
@@ -1,3 +0,0 @@
-﻿. (Join-Path $PSScriptRoot Get-SecDNSLogStatus.ps1)
-. (Join-Path $PSScriptRoot Get-SecWAPS.ps1)
-. (Join-Path $PSScriptRoot Set-SecLogSettings.ps1)
